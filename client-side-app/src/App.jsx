09d4bc298a6839002ed4b7a4cd31d1c8b071b14c
Isha <isha@gmail.com>
2025-04-21 20:24:23 +0530

    added different routes

--- client-side-app/src/App.jsx
+++ client-side-app/src/App.jsx
@@ -1,61 +1,45 @@
 import "./App.css";
 
-import InputFile from "./components/InputFile";
-import Navbar from "./components/Navbar";
-import IngredientList from "./components/IngredientList";
-import { useState } from "react";
+import Navbar from "./components/NavbarElements/Navbar";
+
 import Footer from "./components/Footer";
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+import { About } from "./components/AILogic/About";
+import { Recipe } from "./components/NavbarElements/Recipe";
+import MealRecipe from "./components/NavbarElements/MealRecipe";
 
-function App() {
-  const [ingredientsArray, setIngredientsArray] = useState([]);
-  const [aiResponse, setAIResponse] = useState(null);
-  const [showContent, setShowContent] = useState(false);
-
-  const getAiResponse = async () => {
-    setShowContent(true);
-    try {
-      const response = await fetch("http://localhost:3000", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ ingredients: ingredientsArray }),
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error ${response.status}`);
-      }
-
-      const data = await response.json();
-
-      // ai response
-
-      setAIResponse(data.aiResponse);
-    } catch (error) {
-      console.log("error in Ai response", error);
-    }
-  };
-
-  function addList(nextVal) {
-    if (nextVal.trim() !== "")
-      setIngredientsArray([...ingredientsArray, nextVal.trim()]);
-  }
-
-  console.log(ingredientsArray);
-
+function RootLayout() {
   return (
     <>
       <Navbar />
-
-      <InputFile addList={addList} />
-
-      <IngredientList
-        ingredientsArray={ingredientsArray}
-        getAiResponse={getAiResponse}
-        aiResponse={aiResponse}
-        showContent={showContent}
-      />
-
+      <div className="content">
+        <Outlet /> {/* The *active* page's content will be rendered here */}
+      </div>
       <Footer />
+    </>
+  );
+}
+
+let router = createBrowserRouter([
+  {
+    element: <RootLayout />, //  Use RootLayout here!
+    children: [
+      { path: "/", element: <About /> }, //  Home page
+      { path: "/recipe", element: <Recipe /> }, //  Recipe page
+      { path: "/meal/:id", element: <MealRecipe /> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
     </>
   );
 }
