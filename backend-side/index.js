fe35ff4eb666e926326f809959ed67b171e35b3c
Isha <isha@gmail.com>
2025-04-21 20:23:04 +0530

    added prompt to gemini ai

--- backend-side/index.js
+++ backend-side/index.js
@@ -13,7 +13,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 async function geminiAi(ingredients) {
   try {
-    const prompt = `suppose you are a Michelin star chef give  two recipes which are simple to make and give them in bullet points only in 5-6 points : ${ingredients}`;
+    const prompt = `suppose you are a Michelin star chef give  two recipes which are simple to make and give them in bullet points only in 5-6 points or if they give a name of food please give its recipe both the recipe should be about that food only: ${ingredients}`;
     const result = await model.generateContent(prompt);
     console.log(result.response.text());
     return result.response.text();
