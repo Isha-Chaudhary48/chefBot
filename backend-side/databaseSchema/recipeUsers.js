bf861e65c17785787d7d8d492e89a13fab7d6cfe
Isha <isha@gmail.com>
2025-04-29 17:06:59 +0530

    made a schema for recipes

---
+++ backend-side/databaseSchema/recipeUsers.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import User from "./user.js";
+const recipeUserSchema = new mongoose.Schema(
+  {
+    authorName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    recipeName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    recipeDescription: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    recipeInstruction: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    recipeUrl: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    recipeUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+const RecipeUser = mongoose.model("RecipeUser", recipeUserSchema);
+export default RecipeUser;
