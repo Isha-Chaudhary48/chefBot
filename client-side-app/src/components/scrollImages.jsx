d1a9128251514aa3db5fea033a598139398c4088
Isha <isha@gmail.com>
2025-04-29 17:17:55 +0530

    array of images for marquee

---
+++ client-side-app/src/components/scrollImages.jsx
@@ -0,0 +1,60 @@
+import burger1 from "../assets/burger1.avif";
+import grilledFish from "../assets/grilledFish.jpg";
+import noodles from "../assets/noodles.jpg";
+
+import choclateCake from "../assets/pizzaChef.jpg";
+import pancake1 from "../assets/pancake1.avif";
+import sandwich from "../assets/sandwich.jpg";
+import drinks from "../assets/drinks.jpg";
+import drink2 from "../assets/drink2.jpg";
+import cheeseCake from "../assets/cheeseCake.jpg";
+import sprouts from "../assets/sprouts.jpg";
+import momos from "../assets/momos.jpg";
+
+export let scrollImages = [
+  {
+    id: 1,
+    src: pancake1,
+  },
+  {
+    id: 2,
+    src: burger1,
+  },
+  {
+    id: 3,
+    src: grilledFish,
+  },
+  {
+    id: 4,
+    src: noodles,
+  },
+  {
+    id: 5,
+    src: choclateCake,
+  },
+  {
+    id: 6,
+    src: cheeseCake,
+  },
+  {
+    id: 7,
+    src: momos,
+  },
+  {
+    id: 8,
+    src: drinks,
+  },
+  {
+    id: 9,
+    src: sandwich,
+  },
+  {
+    id: 10,
+    src: drink2,
+  },
+
+  {
+    id: 11,
+    src: sprouts,
+  },
+];
