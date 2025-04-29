f6e6fa28c288b88a2036acf31fdc8a1f67791830
Isha <isha@gmail.com>
2025-04-29 17:06:23 +0530

    in the all the routes for user and its recipes are defined

--- backend-side/index.js
+++ backend-side/index.js
@@ -1,35 +1,118 @@
 import express from "express";
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import "dotenv/config";
 import cors from "cors";
+import geminiAi from "./aiSection.js";
+import connectDB from "./dbConnection.js";
+import User from "./databaseSchema/user.js";
+import RecipeUser from "./databaseSchema/recipeUsers.js";
 
 const app = express();
 app.use(express.json());
-
 app.use(cors());
+connectDB();
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-async function geminiAi(ingredients) {
-  try {
-    const prompt = `suppose you are a Michelin star chef give  two recipes which are simple to make and give them in bullet points only in 5-6 points or if they give a name of food please give its recipe both the recipe should be about that food only: ${ingredients}`;
-    const result = await model.generateContent(prompt);
-    console.log(result.response.text());
-    return result.response.text();
-  } catch (error) {
-    console.log("error in fetching data", error);
-  }
-}
-
-app.post("/", async (req, res) => {
+app.post("/chefBot", async (req, res) => {
   try {
     const ingredients = req.body.ingredients;
     const aiResponse = await geminiAi(ingredients.join(","));
     res.json({ aiResponse: aiResponse });
   } catch (error) {
     console.log("error in Ai processing request", error);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal server error in ai" });
+  }
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const userData = req.body;
+    const userExists = await User.findOne({ email: userData.email });
+    if (userExists) {
+      console.log("user exists");
+      return res.status(200).json({ message: "user already exists" });
+    } else {
+      const newUser = new User({
+        name: userData.name,
+        email: userData.email,
+        picture: userData.picture,
+      });
+      await newUser.save();
+      console.log("user created", newUser);
+      return res.status(200).json({ message: "user created" });
+    }
+  } catch (err) {
+    console.log("error in creating user", err);
+    return res
+      .status(500)
+      .json({ error: "Internal server error in making user", err });
+  }
+});
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log("error in fetching users", err);
+    res
+      .status(500)
+      .json({ error: "internal server error at fetching users", err });
+  }
+});
+
+app.post("/createPost", async (req, res) => {
+  try {
+    const {
+      authorName,
+      recipeName,
+      recipeDescription,
+      recipeInstruction,
+      recipeUrl,
+      recipeUserId,
+    } = req.body;
+    const newRecipeUser = new RecipeUser({
+      recipeUserId: req.body.recipeUserId,
+      authorName: authorName,
+      recipeName: recipeName,
+      recipeDescription: recipeDescription,
+      recipeInstruction: recipeInstruction,
+      recipeUrl: recipeUrl,
+      recipeUserId: recipeUserId,
+    });
+    await newRecipeUser.save();
+    console.log("recipe user created", newRecipeUser);
+    res.status(200).json({ message: "recipe user data saved successfully" });
+  } catch (error) {
+    console.log("error in creating recipe user", error);
+    res
+      .status(500)
+      .json({ error: "recipe user not saved successfully", error });
+  }
+});
+app.get("/postedRecipes", async (req, res) => {
+  try {
+    const getData = await RecipeUser.find().populate(
+      "recipeUserId",
+      "name email picture"
+    );
+
+    console.log("getData successfully", getData);
+    res.status(200).json(getData);
+  } catch (error) {
+    console.log("error in getting data from db", error);
+    res
+      .status(500)
+      .json({ message: "error in getting data from recipeuser db", error });
+  }
+});
+app.get("/usersProfile/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userProfile = await RecipeUser.find({ recipeUserId: id }).populate(
+      "recipeUserId"
+    );
+    res.status(200).json(userProfile);
+  } catch (err) {
+    console.log("error in getting user all recipes", err);
+    res.status(500).json({ error: "error in gettin user all recipes" });
   }
 });
 
