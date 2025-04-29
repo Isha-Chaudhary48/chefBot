458dc97d2973b026d9c829294a006e97acd4bea1
Isha <isha@gmail.com>
2025-04-29 17:08:45 +0530

    add routes to different pages

--- client-side-app/src/App.jsx
+++ client-side-app/src/App.jsx
@@ -12,6 +12,12 @@ import {
 import { About } from "./components/AILogic/About";
 import { Recipe } from "./components/NavbarElements/Recipe";
 import MealRecipe from "./components/NavbarElements/MealRecipe";
+import Dashboard from "./components/NavbarElements/Dashboard";
+import CreatePost from "./components/NavbarElements/CreatePost";
+import UserPostedRecipes from "./components/NavbarElements/UserPostedRecipes";
+import UserAllRecipesSection from "./components/UserAllRecipes/UserAllRecipesSection";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function RootLayout() {
   return (
@@ -29,9 +35,17 @@ let router = createBrowserRouter([
   {
     element: <RootLayout />, //  Use RootLayout here!
     children: [
-      { path: "/", element: <About /> }, //  Home page
+      { path: "/", element: <Dashboard /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/chefBot", element: <About /> }, //  Home page
       { path: "/recipe", element: <Recipe /> }, //  Recipe page
       { path: "/meal/:id", element: <MealRecipe /> },
+      { path: "/createPost", element: <CreatePost /> },
+      {
+        path: "/userPostedRecipes/:id",
+        element: <UserPostedRecipes />,
+      },
+      { path: "/userAllRecipes/:id", element: <UserAllRecipesSection /> },
     ],
   },
 ]);
@@ -40,6 +54,7 @@ function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <ToastContainer />
     </>
   );
 }
