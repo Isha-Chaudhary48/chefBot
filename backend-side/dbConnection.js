dd6f37e73bc6cf06689d60f9d46426e3ea7d2df2
Isha <isha@gmail.com>
2025-05-02 12:41:50 +0530

    made a mongodb connection online

--- backend-side/dbConnection.js
+++ backend-side/dbConnection.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("connected to database");
   } catch (err) {
     console.log("error while connecting to db", err);
